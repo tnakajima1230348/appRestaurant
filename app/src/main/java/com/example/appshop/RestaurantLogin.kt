d6218cc0bcb549d6e3b458ca729a3e610bcd4178
NAKAJIMA <230348e@ugs.kochi-tech.ac.jp>
2022-01-14 15:53:23 +0900

    activity_main.xmlの名前をactivity_restaurant_login.xmlに変更、RestaurantLogin.ktの内容を修正、Restaurant.ktとRestaurantRegisterAccount.ktを追加

--- app/src/main/java/com/example/appshop/RestaurantLogin.kt
+++ app/src/main/java/com/example/appshop/RestaurantLogin.kt
@@ -22,6 +22,89 @@ class RestaurantLogin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_restaurant_login)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        client.connect()
+
+        //edit text, name and password
+        val eTxtUserName: EditText = findViewById(R.id.textBoxUserName)
+        val eTxtPassword: EditText = findViewById(R.id.textBoxPassword)
+
+        val buttonLogin: Button = findViewById(R.id.buttonLogin)
+        val buttonCreateAcc: Button = findViewById(R.id.buttonCreateAccount)
+        //eventListener
+        buttonLogin.setOnClickListener {
+            val loginRequest = JSONObject()
+            val loginParams = JSONObject()
+            val userName: String = eTxtUserName.text.toString()
+            val password: String = eTxtPassword.text.toString()
+            val role = "admin"
+
+            loginParams.put("user_name", userName)
+            loginParams.put("password", password)
+            loginParams.put("role", role)
+
+            loginRequest.put("jsonrpc", "2.0")
+            loginRequest.put("id", loginReqId)
+            loginRequest.put("method", "login")
+            loginRequest.put("params", loginParams)
+            Log.i(javaClass.simpleName, "send login req")
+            Log.i(javaClass.simpleName, loginRequest.toString())
+            client.send(loginRequest.toString())
+        }
+
+        buttonCreateAcc.setOnClickListener {
+            val intent = Intent(this@RestaurantLogin, RestaurantRegisterAccount::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        client = LoginWsClient(this, uri)
+    }
+}
+
+class LoginWsClient(private val activity: Activity, uri: URI) : WsClient(uri){
+
+    private val errorDisplay : TextView by lazy{
+        activity.findViewById(R.id.errorDisplay)
+    }
+
+    override fun onMessage(message: String?) {
+        super.onMessage(message)
+        Log.i(javaClass.simpleName, "msg arrived")
+        Log.i(javaClass.simpleName, "$message")
+        val wholeMsg = JSONObject("$message")
+        val resId: Int = wholeMsg.getInt("id")
+        val result: JSONObject = wholeMsg.getJSONObject("result")
+        val status: String = result.getString("status")
+
+        //if message is about login
+        if(resId == RestaurantLogin.loginReqId){
+            if(status == "success"){
+                val token: String = result.getString("token")
+                val expire: String = result.getString("expire")
+                Log.i(javaClass.simpleName, "login success")
+                Log.i(javaClass.simpleName, "token: $token")
+                Log.i(javaClass.simpleName, "expires in $expire")
+
+                this.close(NORMAL_CLOSURE)
+                activity.runOnUiThread{
+                    val intent = Intent(activity, Restaurant::class.java)
+                    activity.startActivity(intent)
+                }
+
+            }else if(status == "error"){
+                activity.runOnUiThread{
+                    val reason: String = result.getString("reason")
+                    errorDisplay.visibility = View.VISIBLE
+                    Log.i(javaClass.simpleName, "login failed with reason $reason")
+                }
+            }
+        }
     }
 }
