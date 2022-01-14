370431af6c298225a8c4eef58201bf354ffd8de9
NAKAJIMA <230348e@ugs.kochi-tech.ac.jp>
2022-01-14 15:43:42 +0900

    activity_main.xmlを変更

--- app/src/main/java/com/example/appshop/RestaurantLogin.kt
+++ app/src/main/java/com/example/appshop/RestaurantLogin.kt
@@ -1,9 +1,25 @@
 package com.example.appshop
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.net.URI
+import org.json.*
 
 class RestaurantLogin : AppCompatActivity() {
+    companion object{
+        const val loginReqId: Int = 1
+    }
+
+    private val uri = WsClient.serverRemote
+    private var client = LoginWsClient(this, uri)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
