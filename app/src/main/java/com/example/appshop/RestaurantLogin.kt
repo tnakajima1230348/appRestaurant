ded81fea61555f2c01b9c5f8efdade2f8663f8e8
NAKAJIMA <230348e@ugs.kochi-tech.ac.jp>
2022-01-14 15:06:00 +0900

    WsClient.ktを追加、MainActivityをRestaurantLoginに変更、build.gradleの最後に2行追加

--- app/src/main/java/com/example/appshop/RestaurantLogin.kt
+++ app/src/main/java/com/example/appshop/RestaurantLogin.kt
@@ -3,7 +3,7 @@ package com.example.appshop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RestaurantLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
