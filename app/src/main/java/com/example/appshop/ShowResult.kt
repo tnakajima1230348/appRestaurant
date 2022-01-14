e058620a13e435f593ab680d6f359c5495391211
NAKAJIMA <230348e@ugs.kochi-tech.ac.jp>
2022-01-14 16:36:34 +0900

    RestaurantRegisterAccount.ktを修正、ShowResultを追加

---
+++ app/src/main/java/com/example/appshop/ShowResult.kt
@@ -0,0 +1,36 @@
+package com.example.appshop
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_result)
+        val message = intent.getStringExtra("message")
+        val transitionBtnMessage = intent.getStringExtra("transitionBtnMessage")
+        val isBeforeLogin = intent.getBooleanExtra("isBeforeLogin", true)
+
+        val txtMessage: TextView = findViewById(R.id.message)
+        val transitionBtn: Button = findViewById(R.id.transitionButton)
+
+        txtMessage.text = message
+        transitionBtn.text = transitionBtnMessage
+
+        transitionBtn.setOnClickListener {
+            intent = if(isBeforeLogin){
+                Intent(this@ShowResult, RestaurantLogin::class.java)
+            }else{
+                Intent(this@ShowResult, Restaurant::class.java)
+            }
+
+            startActivity(intent)
+        }
+    }
+}
+© 2022 GitHub, Inc.
+Term
