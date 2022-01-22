440c2da86906946047d7d84e8525cb1024869693
NAKAJIMA <230348e@ugs.kochi-tech.ac.jp>
2022-01-22 16:29:10 +0900

    activity_restaurant.xmlを変更

--- app/src/main/java/com/example/appshop/Restaurant.kt
+++ app/src/main/java/com/example/appshop/Restaurant.kt
@@ -1,18 +1,64 @@
 package com.example.appshop
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Button
+import org.json.JSONObject
+import java.net.URI
 
 class Restaurant : AppCompatActivity() {
+    private var restaurant_id: Int = -1
+    private var restaurant_name: String = ""
+    private var email_addr: String = ""
+    private var address: String = ""
+
     companion object{
-        const val RestaurantReqId: Int = 3
+        const val logoutReqId: Int = 3
+        const val getRestaurantInfoId: Int = 7
     }
+
+    private val uri = WsClient.serverRemote
+    private var client = RestaurantTopWsClient(this, uri)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant)
+    }
+
+    private fun fetchClientInfo(client:RestaurantTopWsClient){
+        this.restaurant_id = client.user_id
+        this.restaurant_name = client.user_name
+        this.email_addr = client.email_addr
+        this.address = client.address
+    }
+
+    override fun onResume() {
+        super.onResume()
+        client.connect()
+
+        val token = intent.getStringExtra("token")!!
+        val tokenExpiry = intent.getStringExtra("expire")
+        val restaurantName = intent.getStringExtra("restaurantName")
+        this.restaurant_name = restaurantName!!
+
+        val getRestaurantInfo = Runnable {
+            while (!client.isOpen){
+                //do nothing
+                //wait until websocket open
+            }
+            //the connection openings is guaranteed -> attach no error handler
+            client.sendReqGetRestaurantInfoByName(token, this.restaurant_name)
+            return@Runnable
+        }
+        Thread( getRestaurantInfo ).start()
+
+        Log.i(javaClass.simpleName, "token recved $token")
+        Log.i(javaClass.simpleName, "token expiry $tokenExpiry")
+        Log.i(javaClass.simpleName, "restaurantName: $restaurantName")
 
         val buttonToHome: Button = findViewById(R.id.buttonHome)
         val buttonToAccount: Button = findViewById(R.id.buttonAccount)
